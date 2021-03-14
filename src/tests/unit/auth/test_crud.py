7c67a6f3f27ee3b3dba50c140ca21b05e4922e08
Oleh Pykulytsky <pykulytsky@gmail.com>
2021-03-14 22:56:07 +0200

    add codecov to ci

--- src/tests/unit/auth/test_crud.py
+++ src/tests/unit/auth/test_crud.py
@@ -1,5 +1,7 @@
 from auth import crud
 
+from auth.schemas import UserLogin
+
 
 def test_get_user(user, db):
     assert crud.get_user(db, user.id) == user
@@ -10,4 +12,5 @@ def test_get_user_by_email(user, db):
 
 
 def test_login(user, db):
-    assert crud.
+    user_login = UserLogin(email=user.email, password='1234')
+    assert crud.login(db, user_login) == user
