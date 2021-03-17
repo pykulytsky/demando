b2e831907a5b9d39ffa2ced7f13580244563df3c
Oleh Pykulytsky <pykulytsky@gmail.com>
2021-03-17 22:54:50 +0200

    update base db manager

--- src/base/manager.py
+++ src/base/manager.py
@@ -1,22 +1,35 @@
+from typing import List, Type
+
+from sqlalchemy.orm import Session
 from base.database import SessionLocal
 
 
 class BaseManager():
-    def __init__(self, model) -> None:
+    def __init__(self, model: Type, db: Session = SessionLocal()) -> None:
         self.model = model
-
-    def get_db(self):
-        db = SessionLocal()
-        try:
-            yield db
-        finally:
-            db.close()
-
-    @property
-    def db(self):
-        return self.db
+        self.db = db
 
     def create(self, instance):
+        if not isinstance(instance, self.model):
+            raise TypeError(
+                f"Instance must be {str(self.model)} not {type(instance)}"
+            )
         self.db.add(instance)
         self.db.commit()
         self.db.refresh(instance)
+
+        return instance
+
+    def delete(self, instance):
+        if not isinstance(instance, self.model):
+            raise TypeError(
+                f"Instance must be {str(self.model)} not {type(instance)}"
+            )
+        self.db.delete(instance)
+        self.db.commit()
+        self.db.refresh(instance)
+
+        return instance
+
+    def all(self) -> List[Type]:
+        return self.db.query(self.model).all()
