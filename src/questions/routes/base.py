7d7455b4d665a48b900ce8604a3b50430474ad57
Oleh Pykulytsky <pykulytsky@gmail.com>
2021-03-20 13:59:15 +0200

    base crud router

--- src/questions/routes/base.py
+++ src/questions/routes/base.py
@@ -1,12 +1,10 @@
 from fastapi import APIRouter
 from base.database import engine, Base
 
+from .events import event_router
 from .pools import pools_router
 from .questions import questions_router
-from ..models import Event
-from .. import schemas
 
-from base.router import BaseCrudRouter
 
 
 Base.metadata.create_all(bind=engine)
@@ -17,14 +15,6 @@ router = APIRouter(
     tags=['qa'],
 )
 
-event_router = BaseCrudRouter(
-    model=Event,
-    get_schema=schemas.Event,
-    create_schema=schemas.EventCreate,
-    update_schema=schemas.EventCreate,
-    prefix='/events',
-    tags=['events']
-)
 
 router.include_router(event_router)
 router.include_router(questions_router)
