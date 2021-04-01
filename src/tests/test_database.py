930552c14bf5fdcd5d7e836e365ba37e00648dfb
Oleh Pykulytsky <pykulytsky@gmail.com>
2021-04-01 13:29:56 +0300

    add codecov action to ci

--- src/tests/test_database.py
+++ src/tests/test_database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from base import settings
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.DB_USER}:{settings.DB_USER_PASSWORD}@{settings.DB_HOST}:5432/{settings.TEST_DB_NAME}" # noqa
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.DB_USER}:{settings.DB_USER_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.TEST_DB_NAME}" # noqa
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
