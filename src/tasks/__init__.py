d16ec52b27860a924f3578022a540457272c4559
Oleh Pykulytsky <pykulytsky@gmail.com>
2022-04-18 12:47:54 +0300

    :sparkles: update celery config

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -1,4 +1,6 @@
 from celery import Celery
 
+
 app = Celery("tasks", broker="redis://localhost:6379")
+app.config_from_object('tasks.celeryconfig')
 app.autodiscover_tasks()
---
+++ src/tasks/mails.py
@@ -0,0 +1,6 @@
+from tasks import app
+
+
+@app.task
+def send_verification_mail():
+    pass
